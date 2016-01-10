e6ba83bca71e8c8cc29246615e9681e1ea24b8d5
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-01-09 23:35:23 -0700

    iteration clean phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,8 +8,37 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+  if phone_number < 10
+    phone_number = "bad number"
+  elsif phone_number == 10
+    phone_number
+  elsif phone_number == 11
+    if phone_number[0] == 1
+      phone_number = phone_number.to_s
+      phone_number[0] = ''
+      phone_number = phone_number.to_i
+    else
+      phone_number = "bad number"
+    end
+  else
+
+  end
+
+end
+
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts "EventManager Initialized!"
@@ -20,12 +49,17 @@ template_letter = File.read 'form_letter.erb'
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
+
+  phone_number = row[:homephone]
+
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
+  save_thank_you_letters(id, form_letter)
   puts form_letter
 end
